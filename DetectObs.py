9292a1dc19c6f62de70e39bb822d1ea7c490f25a
MsLiorBracha <liorbracha53@gmail.com>
2021-11-02 11:09:34 +0200

    Convert image to cv2

---
+++ DetectObs.py
@@ -0,0 +1,26 @@
+import cv2
+import sys
+import numpy as np
+
+
+def detect(data):
+    nparr = np.frombuffer(data, dtype=np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    boundaries = [
+        ([17, 15, 100], [50, 56, 200]),
+        ([86, 31, 4], [220, 88, 50]),
+        ([25, 146, 190], [62, 174, 250]),
+        ([103, 86, 65], [145, 133, 128])
+    ]
+
+    # loop over the boundaries
+    for (lower, upper) in boundaries:
+        # create NumPy arrays from the boundaries
+        lower = np.array(lower, dtype="uint8")
+        upper = np.array(upper, dtype="uint8")
+        # find the colors within the specified boundaries and apply
+        # the mask
+        mask = cv2.inRange(image, lower, upper)
+        output = cv2.bitwise_and(image, image, mask=mask)
+        # show the images
+        cv2.imshow("images", np.hstack([image, output]))
